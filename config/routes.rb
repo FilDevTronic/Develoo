b4c7be652a030542a930dd71139a6ec765dcf97d
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-01 18:15:00 +1000

    Added Landing, FAQ and Contact page templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root 'profiles#index'
-  resources :profiles
   devise_for :users
+
+  root 'pages#landing'
+
+  get 'pages/landing', as: 'welcome'
+
+  get 'pages/contact', as: 'contact'
+
+  get 'pages/faq', as: 'faq'
+
+  resources :profiles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get landing" do
+    get pages_landing_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get pages_faq_url
+    assert_response :success
+  end
+
+end
