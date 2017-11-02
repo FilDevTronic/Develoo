f9ff6004aaac13fba368c4931810c50b3c6b7cd9
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-03 01:12:54 +1000

    Added sent messages inbox, trash, deleting and restoring messages, marking conversations as read

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,12 +1,8 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :get_box, only [:index]
   before_action :get_mailbox
-  before_action :get_conversation, except: [:index]
-
-  def index
-    @conversations = @mailbox.inbox.paginate(page: params[:page], per_page: 10)
-  end
+  before_action :get_conversation, except: [:index, :empty_trash]
+  before_action :get_box, only: [:index]
 
   def index
     if @box.eql? "inbox"
@@ -28,6 +24,32 @@ class ConversationsController < ApplicationController
     current_user.reply_to_conversation(@conversation, params[:body])
     flash[:success] = 'Reply sent'
     redirect_to conversation_path(@conversation)
+  end
+
+  def destroy
+    @conversation.move_to_trash(current_user)
+    flash[:success] = 'The conversation was moved to trash.'
+    redirect_to conversations_path
+  end
+
+  def restore
+    @conversation.untrash(current_user)
+    flash[:success] = 'The conversation was restored.'
+    redirect_to conversations_path
+  end
+
+  def empty_trash
+    @mailbox.trash.each do |conversation|
+      conversation.receipts_for(current_user).update_all(deleted: true)
+    end
+    flash[:success] = 'Your trash was cleaned!'
+    redirect_to conversations_path
+  end
+
+  def mark_as_read
+    @conversation.mark_as_read(current_user)
+    flash[:success] = 'The conversation was marked as read.'
+    redirect_to conversations_path
   end
 
   private
