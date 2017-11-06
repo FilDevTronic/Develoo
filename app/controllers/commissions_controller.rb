ff20285869c54b0f2120ba7ace08bc6e2867714e
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-06 23:50:01 +1000

    Made users have to authenticate before being able to CUD Commissions and User Stories

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -1,4 +1,5 @@
 class CommissionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_commission, only: [:show, :edit, :update, :destroy]
 
   # GET /commissions
