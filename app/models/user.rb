e43010bde44f4bccfdb68cf75373e228db03b6ce
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-07 23:52:07 +1000

    Gave users a default role :user, fixed commissions list to be viewable by unregistered users, added sign in/up link to commissions for users to see details and purchase page for individual commissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
 class User < ApplicationRecord
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  after_create :create_profile
 
   validates :name, presence: true
 
   validates :alias, presence: true
+
+  after_create :assign_default_role
+
+  after_create :create_profile
 
   has_one :profile, dependent: :destroy
 
   has_many :commission, dependent: :destroy
 
   acts_as_messageable
+
+  def assign_default_role
+    add_role(:user)
+  end
 end
