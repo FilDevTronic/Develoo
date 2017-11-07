8ece4d28737588f52c922ac2a325e2bbf041fc1a
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-07 12:14:52 +1000

    Added a hidden column to commissions, to archive rather than delete them initially

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107012038) do
+ActiveRecord::Schema.define(version: 20171107021343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171107012038) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "hidden", default: false
     t.index ["user_id"], name: "index_commissions_on_user_id"
   end
 
