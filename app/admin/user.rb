b1ceecccb4f141e77ad2270f561ebf9b09ed2b6d
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-10 04:16:13 +1000

    Added ability for Admins to view, message directly, and edit client user profiles through the website, including changing roles. Also enabled ability to change roles,including changing an user into an admin from ActiveAdmin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -11,4 +11,29 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  permit_params :alias, :name, :email, :password, :password_confirmation, role_ids: []
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :alias
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :roles, as: :check_boxes
+    end
+    f.actions
+  end
+
+  controller do
+    def update
+      if params[:user][:password].blank?
+        params[:user].delete "password"
+        params[:user].delete "password_confirmation"
+      end
+
+      super
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order('created_at DESC').paginate(page: params[:page], per_page: 30)
+    @users = User.order('alias ASC').paginate(page: params[:page], per_page: 30)
+    @profile = Profile.all
   end
 end
