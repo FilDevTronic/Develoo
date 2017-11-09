f3d56f59919f43a715eb26a53ca7bba62b53ce39
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-10 07:22:30 +1000

    Enabled devise mailers for email and password change, unlock, etc, changed mailer settings in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-    api_key: 'api-myapikey',
-    domain: 'mydomain.com'
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain: ENV['MAILGUN_DOMAIN']
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
